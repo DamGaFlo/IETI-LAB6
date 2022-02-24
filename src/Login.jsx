2d9c388590864ae3bc7bb52f416a432292391b80
DamGaFlo <jd98091819@gmail.com>
2022-02-24 03:27:39 -0500

    refactorizacion de componentes innecesarios

---
+++ src/Login.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Stack } from "@mui/material";
+import { Container } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+
+function Login() {
+  const handleSubmit = (event) => {
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="Username"
+            label="Username"
+            name="Username"
+            autoComplete="Username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.jsx";
+import Login from "./Login.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById("root")
 );
