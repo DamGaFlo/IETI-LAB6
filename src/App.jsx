dc22aba60e5e4cdae2a14c57ff8861721aade55c
DamGaFlo <jd98091819@gmail.com>
2022-02-23 23:57:31 -0500

    primer componente creado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ButtonStandar from "./componets/ButtonStandar";
+
+function App() {
+  return (
+    <div>
+      <ButtonStandar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/ButtonStandar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+function ButtonStandar(props) {
+  return (
+    <Button color="primary" variant="contained">
+      Daam
+    </Button>
+  );
+}
+export default ButtonStandar;
